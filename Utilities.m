c80e0df745d038c92910bfa838f48fd7b8025f76
maxbernal <jose.bernal@correounivalle.edu.co>
2016-05-05 01:44:03 +0200

    Uploading part 3

--- Utilities.m
+++ Utilities.m
@@ -9,12 +9,32 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function loc = evaluate_sift(image1, image2, H)
+% Function handlers
+function h = Utilities()
+    h.evaluate_sift_one_scale = @evaluate_sift_one_scale;
+    h.evaluate_normal_sift = @evaluate_normal_sift;
+end
+
+function loc = evaluate_sift_one_scale(image1, image2, H)
+    loc = evaluate_sift(image1, image2, H, true);
+end
+
+function loc = evaluate_normal_sift(image1, image2, H)
+    loc = evaluate_sift(image1, image2, H, false);
+end
+
+function loc = evaluate_sift(image1, image2, H, is_one_scale)
     I1 = single(rgb2gray(image1));
     I2 = single(rgb2gray(image2));
     
-    [f1, d1] = vl_sift(I1);
-    [f2, d2] = vl_sift(I2);
+    if ~is_one_scale
+        [f1, d1] = vl_sift(I1);
+        [f2, d2] = vl_sift(I2);
+    else
+        [f1, d1] = vl_sift(I1, 'Octaves', 1);
+        [f2, d2] = vl_sift(I2, 'Octaves', 1);
+    end
+    
     matches = vl_ubcmatch(d1, d2);
     
     loc = 0;
--- part_3_evaluation_script.m
+++ part_3_evaluation_script.m
@@ -2,18 +2,21 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%                                                                     %%%
 %%% Description: VP Lab 4 SIFT                                          %%%
-%%%              Part 2 - Evaluate SIFT                                 %%%
+%%%              Part 3 - Comparison                                    %%%
 %%% Authors: Rodrigo Daubt and Jose Bernal                              %%%
 %%% Date: 21-04-2016                                                    %%%
 %%%                                                                     %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-clc;
-close all;
 clear all;
+close all;
+clc;
 
 util = Utilities;
+
+image1 = imread('SEQUENCE2/Image_00a.png');
+image2 = imread('SEQUENCE2/Image_01a.png');
 
 noises = {'a', 'b', 'c', 'd'};
 colors = {'r', 'b', 'g', 'c'};
@@ -28,11 +31,15 @@ figure(1); hold on; grid on;
 xlim([1 size(Sequence1Homographies, 2)]);
 
 for noise = 1 : length(noises)
-    results = zeros(size(Sequence1Homographies, 2), 1);
+    results = zeros(size(Sequence1Homographies, 2), 2);
     for i = 1 : size(Sequence1Homographies, 2)
         I2 = imread(strcat('SEQUENCE1/Image_', num2str(i,'%.2u'), noises{noise},'.png'));
 
-        results(i, :) = util.evaluate_normal_sift(I1, I2, Sequence3Homographies(i).H);
+        loc1 = util.evaluate_normal_sift(I1, I2, Sequence1Homographies(i).H);
+        loc2 = util.evaluate_sift_one_scale(I1, I2, Sequence1Homographies(i).H);
+
+        results(i, 1) = loc1;
+        results(i, 2) = loc2;
     end
 
     style = strcat(forms{noise}, '-', colors{noise});
@@ -52,7 +59,11 @@ for noise = 1 : length(noises)
     for i = 1 : size(Sequence2Homographies, 2)
         I2 = imread(strcat('SEQUENCE2/Image_', num2str(i,'%.2u'), noises{noise},'.png'));
 
-        results(i, :) = util.evaluate_normal_sift(I1, I2, Sequence3Homographies(i).H);
+        loc1 = util.evaluate_normal_sift(I1, I2, Sequence1Homographies(i).H);
+        loc2 = util.evaluate_sift_one_scale(I1, I2, Sequence1Homographies(i).H);
+
+        results(i, 1) = loc1;
+        results(i, 2) = loc2;
     end
 
     style = strcat(forms{noise}, '-', colors{noise});
@@ -72,7 +83,11 @@ for noise = 1 : length(noises)
     for i = 1 : size(Sequence3Homographies, 2)
         I2 = imread(strcat('SEQUENCE3/Image_', num2str(i,'%.2u'), noises{noise}, '.png'));
 
-        results(i, :) = util.evaluate_normal_sift(I1, I2, Sequence3Homographies(i).H);
+        loc1 = util.evaluate_normal_sift(I1, I2, Sequence1Homographies(i).H);
+        loc2 = util.evaluate_sift_one_scale(I1, I2, Sequence1Homographies(i).H);
+
+        results(i, 1) = loc1;
+        results(i, 2) = loc2;
     end
 
     style = strcat(forms{noise}, '-', colors{noise});
